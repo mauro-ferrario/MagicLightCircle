e16ecb668d202349df0578cfd13ed1eb5c67a0a1
mauro-ferrario <hello@mauroferrario.com>
2015-11-11 21:35:25 +0100

    Create basic structure

--- src/MagicLightCircle.cpp
+++ src/MagicLightCircle.cpp
@@ -7,3 +7,65 @@
 //
 
 #include "MagicLightCircle.h"
+
+
+
+MagicLightCircle::MagicLightCircle()
+{
+  
+}
+
+MagicLightCircle::~MagicLightCircle()
+{
+  
+}
+
+void MagicLightCircle::setCircleResolution(int num)
+{
+  
+}
+
+void MagicLightCircle::setup(int resolution)
+{
+  for(int a = 0; a < resolution; a++)
+  {
+    addNewMagicPoint();
+  }
+}
+
+void MagicLightCircle::addNewMagicPoint()
+{
+  MagicPoint* p = new MagicPoint();
+  magicPoints.push_back(p);
+  totMagicPoints++;
+  p = NULL;
+}
+
+void MagicLightCircle::update()
+{
+  
+}
+
+void MagicLightCircle::draw()
+{
+  float radius = 100;
+  float angle = 0;
+  ofPushStyle();
+  ofNoFill();
+  ofRect(0,0,radius*2, radius*2);
+  ofSetColor(255);
+  ofPushMatrix();
+  ofTranslate(radius, radius);
+  ofCircle(0, 0, 100);
+  for(int a = 0; a < totMagicPoints; a++)
+  {
+    ofVec2f pos;
+    pos.x = (cos(ofDegToRad(angle)) * radius);// + radius*.5;
+    pos.y = (sin(ofDegToRad(angle)) * radius);// + radius*.5;
+    magicPoints[a]->draw(pos);
+    angle += float(360/totMagicPoints);
+    cout << angle << endl;
+  }
+  ofPopStyle();
+  ofPopMatrix();
+}
--- src/MagicLightCircle.h
+++ src/MagicLightCircle.h
@@ -9,6 +9,68 @@
 #ifndef __MagicLightCircle__MagicLightCircle__
 #define __MagicLightCircle__MagicLightCircle__
 
-#include <stdio.h>
+#include "ofMain.h"
+
+
+class MagicPoint
+{
+public:
+        MagicPoint()
+  {
+    
+  }
+        ~MagicPoint()
+  {
+    
+  }
+  
+  void  setup()
+  {
+    
+  }
+  
+  void  update()
+  {
+    
+  }
+  
+  void  draw(ofVec2f pos)
+  {
+    ofPushMatrix();
+    ofTranslate(pos);
+    ofPushStyle();
+    ofNoFill();
+    ofSetColor(255);
+    ofCircle(5,5,10,10);
+    ofFill();
+    ofSetColor(255,0,0);
+    ofCircle(5,5,10,10);
+    ofPopStyle();
+    ofPopMatrix();
+  }
+  
+private:
+  int   id;
+  float intensity;
+};
+
+
+class MagicLightCircle
+{
+public:
+                      MagicLightCircle();
+                      ~MagicLightCircle();
+  
+  void                setup(int resolution);
+  void                update();
+  void                draw();
+
+private:
+  void                addNewMagicPoint();
+  void                setCircleResolution(int num);
+  int                 resolution;
+  int                 totMagicPoints;
+  vector<MagicPoint*> magicPoints;
+};
 
 #endif /* defined(__MagicLightCircle__MagicLightCircle__) */
