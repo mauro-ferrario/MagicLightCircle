51cdf0eef5361973aaed538530a9bb5d2a6189b7
mauro-ferrario <hello@mauroferrario.com>
2015-11-11 21:09:29 +0100

    Firstr commit

---
+++ src/MagicLightCircle.cpp
@@ -0,0 +1,9 @@
+//
+//  MagicLightCircle.cpp
+//  MagicLightCircle
+//
+//  Created by Mauro Ferrario on 11/11/15.
+//
+//
+
+#include "MagicLightCircle.h"
---
+++ src/MagicLightCircle.h
@@ -0,0 +1,14 @@
+//
+//  MagicLightCircle.h
+//  MagicLightCircle
+//
+//  Created by Mauro Ferrario on 11/11/15.
+//
+//
+
+#ifndef __MagicLightCircle__MagicLightCircle__
+#define __MagicLightCircle__MagicLightCircle__
+
+#include <stdio.h>
+
+#endif /* defined(__MagicLightCircle__MagicLightCircle__) */
