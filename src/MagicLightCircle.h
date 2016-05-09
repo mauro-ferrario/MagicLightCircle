e66bcd0e71ead2b5be6dcb3c557c66b6484872a3
mauro-ferrario <hello@mauroferrario.com>
2016-05-09 12:34:42 +0200

    Refactoring

--- src/MagicLightCircle.h
+++ src/MagicLightCircle.h
@@ -87,7 +87,6 @@ private:
   void                turnOnLight(int lightId, float newIntensityValue = 1);
   std::tr1::unordered_map<string, ofParameter<float>* > mapToOfParameterFloatValue;
   std::tr1::unordered_map<string, float >  multipliers;
-
   PixelMappingCircle  pixelMapping;
   void                setupPixelMapping();
 };
